698171ec7068d9511f5842be18d0bd766ea15fbe
Yeshiwas Dagnaw <58876309+Yeshiwas21@users.noreply.github.com>
2025-12-24 18:30:57 +0300

    Delete frappetheme/setup.py

--- frappetheme/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="erp_lite",            # The app name, used by Bench
-    version="0.0.1",            # Your app version
-    packages=find_packages(),    # Finds your erp_lite/ folder automatically
-    include_package_data=True,   # Includes non-Python files (like hooks.py, public/)
-    install_requires=[],         # Add dependencies if needed, e.g., ["frappe"]
-)
